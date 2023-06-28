412a754961bbf2c5addab2755394401109524af6
vedre77 <vedranvitas@net.hr>
2023-06-28 08:11:13 +0200

    Edited allowed requests for reviews.

--- src/main/java/com/love2code/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/love2code/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.love2code.springbootlibrary.config;
 
 import com.love2code.springbootlibrary.entitiy.Book;
+import com.love2code.springbootlibrary.entitiy.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -22,8 +23,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 HttpMethod.PUT};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
