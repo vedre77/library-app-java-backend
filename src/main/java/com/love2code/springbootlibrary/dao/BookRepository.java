0084cabd27b3d9630157bb4e4d2085ddaed72bb3
vedre77 <vedranvitas@net.hr>
2023-07-03 06:08:01 +0200

    Added find books by ID list

--- src/main/java/com/love2code/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/love2code/springbootlibrary/dao/BookRepository.java
@@ -4,7 +4,11 @@ import com.love2code.springbootlibrary.entitiy.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
@@ -13,4 +17,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     /*search by category*/
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
+
+    @Query("SELECT b FROM Book b WHERE b.id IN :book_ids")
+    List<Book> findBooksByIds(@Param("book_ids") List<Long> bookId);
+
 }
