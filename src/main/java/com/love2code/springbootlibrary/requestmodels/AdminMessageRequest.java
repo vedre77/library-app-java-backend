99d389d04d8631dc22e5bdc21088800f1ab61964
vedre77 <vedranvitas@net.hr>
2023-07-04 13:36:35 +0200

    Added admin requests.

---
+++ src/main/java/com/love2code/springbootlibrary/requestmodels/AdminMessageRequest.java
@@ -0,0 +1,11 @@
+package com.love2code.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AdminMessageRequest {
+
+    private Long id;
+    private String response;
+}
+
