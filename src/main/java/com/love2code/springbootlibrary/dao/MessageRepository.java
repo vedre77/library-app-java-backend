4dabeba0601341e3ed2415e9bbaca7ac04392e61
vedre77 <vedranvitas@net.hr>
2023-07-04 13:36:54 +0200

    Added method to retrieve Message IDs.

--- src/main/java/com/love2code/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/love2code/springbootlibrary/dao/MessageRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
-    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
-
+    Page<Message> findByClosed(boolean closed, Pageable pageable);
+    List<Message> findByClosedFalse();
 }
